6881fe04d0088e58c397c1d28767e5150f0be201
Damir <damirkzzn@gmail.com>
2022-06-25 09:23:46 +0300

    feat: update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+  if(n<0){
+    n=n*(-1);
+  }
+  n=n+"";
+  var i=0;
+  var n1="";
+  while(i<n.length){
+    n1= n.charAt(i)+n1 
+    i=i+1;
+  }
+  n1=(n1+1-1)/10;
+  return n1;
 }
